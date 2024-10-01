7b758be41dcfc5a356b6a00bc35ef8c5a2040fcc
tang <yongjian.tang@tum.de>
2024-10-01 13:20:36 +0200

    evaluate the next half part of llama31

--- llama/llama_31_download.py
+++ llama/llama_31_download.py
@@ -49,114 +49,112 @@ pipe = pipeline(
 # path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_20_bi.txt'
 # save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_40_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_40_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_40_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_40_bi.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_80_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_80_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_80_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_80_bi.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_160_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_160_bi.txt'
-save_completion_list(path, completion_list)
-
-
-################# promise binary embedding
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_0_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_0_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_5_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_5_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_10_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_10_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_20_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_20_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_40_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_40_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_80_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_80_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_160_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_160_bi.txt'
-save_completion_list(path, completion_list)
-
-################# promise binary tfidf
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_0_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_0_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_5_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_5_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_10_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_10_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_20_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_20_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_40_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_40_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_80_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_80_bi.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_160_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_160_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_160_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_160_bi.txt'
+# save_completion_list(path, completion_list)
 
 
+# ################# promise binary embedding
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_0_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_0_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_5_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_5_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_10_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_10_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_20_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_20_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_40_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_40_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_80_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_80_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_160_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_160_bi.txt'
+# save_completion_list(path, completion_list)
+
+# ################# promise binary tfidf
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_0_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_0_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_5_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_5_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_10_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_10_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_20_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_20_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_40_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_40_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_80_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_80_bi.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_tfidf_160_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_tfidf_160_bi.txt'
+# save_completion_list(path, completion_list)
 
 
 
@@ -168,82 +166,84 @@ save_completion_list(path, completion_list)
 
 
 
-################# promise multiclass random
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_0.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_0.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_5.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_5.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_10.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_10.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_20.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_20.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_40.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_40.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_80.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_80.txt'
-save_completion_list(path, completion_list)
-
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_160.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_160.txt'
-save_completion_list(path, completion_list)
 
 
-################# promise multi-class embedding
+# ################# promise multiclass random
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_0.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_0.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_0.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_0.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_5.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_5.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_5.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_5.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_10.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_10.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_10.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_10.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_20.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_20.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_20.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_20.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_40.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_40.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_40.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_40.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_80.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_80.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_160.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_160.txt'
+# save_completion_list(path, completion_list)
+
+
+# ################# promise multi-class embedding
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_0.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_0.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_5.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_5.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_10.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_10.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_20.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_20.txt'
+# save_completion_list(path, completion_list)
+
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_40.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_embedding_40.txt'
+# save_completion_list(path, completion_list)
 
 path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_embedding_80.txt'
 prompt_list = read_prompt_list(path)
