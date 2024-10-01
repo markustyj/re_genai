0f69d005b6526f770a67706182624540ec63df9a
tang <yongjian.tang@tum.de>
2024-10-01 11:03:40 +0200

    allow more GPU to get the completion from llama 31 based on more few-shot examples

--- llama/llama_31_download.py
+++ llama/llama_31_download.py
@@ -6,6 +6,9 @@
 
 # 0: availability, 1: performance, 2: maintainability, 3: portability, 4: scalability, 5: security, 6: fault tolerance
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"  #"1"  or "0,1" for multiple GPUs
+
 import torch
 from transformers import pipeline
 from utils_llama import read_prompt_list, get_completion_list, save_completion_list
@@ -22,29 +25,29 @@ pipe = pipeline(
 
 ################# promise binary random
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_0_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_0_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_0_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_0_bi.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_5_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_5_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_5_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_5_bi.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_10_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_10_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_10_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_10_bi.txt'
+# save_completion_list(path, completion_list)
 
-path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_20_bi.txt'
-prompt_list = read_prompt_list(path)
-completion_list = get_completion_list(pipe, prompt_list)
-path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_20_bi.txt'
-save_completion_list(path, completion_list)
+# path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_20_bi.txt'
+# prompt_list = read_prompt_list(path)
+# completion_list = get_completion_list(pipe, prompt_list)
+# path = '/home/z004r5cc/re_genai/completions/llama31_8b/completion_promise_random_20_bi.txt'
+# save_completion_list(path, completion_list)
 
 path = '/home/z004r5cc/re_genai/processed_prompts/prompt_promise_random_40_bi.txt'
 prompt_list = read_prompt_list(path)
